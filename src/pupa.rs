6949ae37c1fb76ac7a93b757f73da61c56d1b990
Guillaume Quintard <guillaume@varnish-software.com>
2016-03-31 16:53:22 +0200

    Rewrite a few things

--- src/pupa.rs
+++ src/pupa.rs
@@ -13,46 +13,7 @@ use std::fs::File;
 use std::sync::{Arc, Mutex};
 use std::boxed::Box;
 use std::collections::HashMap;
-
-#[repr(C)]
-pub struct VrtCtx<'a> {
-	magic: c_uint,
-	method: c_uint,
-	handling: *const c_uint,
-
-	vsb: *const c_void,
-	vsb_log: *const c_void,
-	vcl: *const c_void,
-	ws: &'a ws,
-
-	req: *const c_void,
-	http_req: *const c_void,
-	http_req_top: *const c_void,
-	http_resp: *const c_void,
-
-	bo: *const c_void,
-	http_bereq: *const c_void,
-	http_beresp: *const c_void,
-
-	now: c_double,
-
-	specific: *const c_void
-}
-
-#[repr(C)]
-struct ws {                                                                                                                                                                                                        
-	magic: c_uint,
-	id: [c_char; 4], //id[4], actually
-	s: *const c_char,
-	f: *mut c_char,
-	r: *const c_char,
-	e: *const c_char,
-}
-
-extern {
-	fn WS_Reserve(ws: *const ws, bytes: c_uint) -> c_uint;
-	fn WS_Release(ws: *const ws, bytes: c_uint);
-}
+use std::collections::hash_map::Entry::{Occupied, Vacant};
 
 #[repr(C)]
 pub enum VclEvent {
@@ -69,17 +30,8 @@ pub struct vmod_priv {
 	free	: *const c_void
 }
 
-fn conv(input: *const c_char) -> String {
-	unsafe { 
-		CString::new(String::new() +
-			     CStr::from_ptr(input).to_str().unwrap())
-	}.unwrap()
-	.into_string()
-	.unwrap()
-}
-
 #[no_mangle]
-pub extern fn init_function(_	: *const VrtCtx,
+pub extern fn init_function(_	: *const c_void,
 			    prv	: &mut vmod_priv,
 			    ev	: VclEvent ) -> c_int{
 	match ev {
@@ -95,18 +47,61 @@ pub extern fn init_function(_	: *const VrtCtx,
 	0
 }
 
+fn conv(input: *const c_char) -> String {
+	unsafe {
+		CString::new(String::new() +
+			     CStr::from_ptr(input).to_str().unwrap())
+	}.unwrap()
+	.into_string()
+	.unwrap()
+}
+
 #[no_mangle]
-pub unsafe extern fn vmod_push(ctx	: &VrtCtx,
+pub unsafe extern fn vmod_push(_	: *const c_void,
 				prv	: &vmod_priv,
 				input	: *const c_char) -> c_int {
 	
-	let mut hash = unsafe { &*prv.prv }
-			.lock()
-			.unwrap();
-	
+	let mut hash = unsafe { &*prv.prv }.lock().unwrap();
+
 	let key = conv(input);
 
 	let c = hash.entry(key).or_insert(0);
 	*c += 1;
 	*c
+}
+
+#[no_mangle]
+pub unsafe extern fn vmod_pull(_	: *const c_void,
+				prv	: &vmod_priv,
+				input	: *const c_char) -> c_int {
+
+	let mut hash = unsafe { &*prv.prv }.lock().unwrap();
+
+	let key = conv(input);
+
+	match hash.entry(key) {
+		Occupied(mut entry) => {
+			if *entry.get() == 0 { 0 }
+			else {
+				*entry.get_mut() -= 1;
+				*entry.get()
+			}
+		},
+		Vacant(entry) => 0
+	}
+}
+
+#[no_mangle]
+pub unsafe extern fn vmod_peek(_	: *const c_void,
+				prv	: &vmod_priv,
+				input	: *const c_char) -> c_int {
+
+	let mut hash = unsafe { &*prv.prv }.lock().unwrap();
+
+	let key = conv(input);
+
+	match hash.entry(key) {
+		Occupied(entry) => *entry.get(),
+		Vacant(entry) => 0
+	}
 }
